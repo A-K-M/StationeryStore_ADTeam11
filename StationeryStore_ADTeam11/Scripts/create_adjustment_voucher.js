a0a2f73412a6ee75fec10e3dc49760094d7b3dfc
thantnaylin <thantnaycode@gmail.com>
2019-08-01 00:23:21 +0800

    Create adjustment voucher still in progress

--- StationeryStore_ADTeam11/Scripts/create_adjustment_voucher.js
+++ StationeryStore_ADTeam11/Scripts/create_adjustment_voucher.js
@@ -25,3 +25,110 @@ function loadItems(CategoryID) {
         }
     });
 }
+
+//functions for data table manipulation
+function addItem() {
+
+    //if (isNaN(quantity.value) || quantity.value == '' || item.value == 0 || reason.value == '') {
+
+    //    alert("Please input valid data!");
+
+    //    return false;
+    //}
+
+    let row = `
+                <tr>
+                    <td><input type="hidden" value="${$('#item option:selected').val()}">${$('#item option:selected').text()}</td>
+                    <td><input type="text" value="${$('#quantity').val()}"></td>
+                    <td>${$('#reason').val()}</td>
+                    <td><button class="btn btn-outline-danger btn-sm" onclick="">Remove</button></td>
+                </tr>`;
+
+    $(row).insertAfter("#append-node");
+
+    let btnSubmit = `<div class="col-6">
+                        <div class="row">
+                            <div class="col-6"></div>
+                            <div class="col-3">
+                            <button class="btn btn-outline-success">Submit</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>`;
+
+    $("#item").val("0");
+    $("#quantity").val("");
+    $("#reason").val("");
+
+    let rowCount = $("#data-body tbody tr").length -1 ;
+
+    if (rowCount > 0) {
+
+        let btnSubmit = `
+                    <div class="row">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6"></div>
+                            <div class="col-3">
+                            <button class="btn btn-outline-success">Submit</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>`;
+
+        $(btnSubmit).insertAfter('submit-append-node');
+    }
+}
+
+
+
+//const addItem = () => {
+
+//    counter += 1;
+
+//    let trId = 'tr' + counter;
+
+//    let item = document.getElementById('item');
+//    let quantity = document.getElementById('quantity');
+//    let reason = document.getElementById('reason');
+
+//    if (isNaN(quantity.value) || quantity.value == '' || item.value == 0 || reason.value == '') {
+
+//        alert("Please input valid data!");
+
+//        return false;
+//    }
+
+//    let row = `
+//           <tr id="${trId}">
+//                <td><input type="hidden" value="${item.options[item.selectedIndex].value}">${item.options[item.selectedIndex].text}</td>
+//                <td><input type="text" value="${quantity.value}"></td>
+//                <td>${reason.value}</td>
+//                <td><button class="btn btn-outline-danger btn-sm" onclick="removeItem(${trId})">Remove</button></td>
+//           </tr>`;
+
+//    let btnSubmit = `<div class="col-6">
+//                            <div class="row">
+//                              <div class="col-6"></div>
+//                              <div class="col-3">
+//                                <button class="btn btn-outline-success">Submit</button>
+//                              </div>
+//                            </div>
+//                          </div>
+//                        </div>`;
+
+//    let itemData = document.getElementById('item-data');
+
+//    itemData.innerHTML += row;
+
+//    item.options[0].selected = true;
+//    quantity.value = null;
+//    reason.value = null;
+
+//    if (checkTableRow()) {
+
+//        let placeholder = document.getElementById('submit-placeholder');
+//        placeholder.innerHTML = btnSubmit;
+//    }
+//};
