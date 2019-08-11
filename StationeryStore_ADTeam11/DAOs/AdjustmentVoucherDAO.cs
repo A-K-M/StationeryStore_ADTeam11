f3683505817e859c160d80010a35c3bafb7b8c5f
thantnaylin <thantnaycode@gmail.com>
2019-08-11 12:25:11 +0800

    Some changes to AVDAO

--- StationeryStore_ADTeam11/DAOs/AdjustmentVoucherDAO.cs
+++ StationeryStore_ADTeam11/DAOs/AdjustmentVoucherDAO.cs
@@ -101,6 +101,14 @@ namespace StationeryStore_ADTeam11.DAOs
 
             itemsAbove250 = itemList.Where(x => (x.FirstPrice + x.SecondPrice + x.ThirdPrice) / 3 >= 250).ToList();
 
+
+            //List<AdjustmentVoucherVM> result = VoucherItemPriceHelper(voucherList, itemVoucherList, itemsAbove250, employeeList);
+
+            //return result;
+
+            List<AdjustmentVoucherVM> vouchersVMList = new List<AdjustmentVoucherVM>();
+            AdjustmentVoucherVM voucherVM = null;
+            
             List<int> voucherIdsForManager = (from voucherDetail in itemVoucherList
                                               join items in itemsAbove250
                                               on voucherDetail.ItemId equals items.Id
@@ -111,6 +119,160 @@ namespace StationeryStore_ADTeam11.DAOs
                 voucherList.RemoveAll(x => x.Id == id);
             }
 
+            voucherList = voucherList.GroupBy(x => x.Id)
+              .Select(y => y.First())
+              .ToList();
+
+            foreach (AdjustmentVoucher voucher in voucherList)
+            {
+                employee = employeeList.Find(x => x.Id == voucher.EmployeeId);
+
+                voucherVM = new AdjustmentVoucherVM()
+                {
+                    Name = employee.Name,
+                    Id = voucher.Id,
+                    Date = voucher.Date,
+                    Status = voucher.Status,
+                    TotalQuantity = itemVoucherList.Where(x => x.VoucherId == voucher.Id)
+                                    .Sum(y => y.Quantity)
+                };
+
+                vouchersVMList.Add(voucherVM);
+            }
+
+            return vouchersVMList;
+        }
+
+        public List<AdjustmentVoucherVM> VoucherItemPriceHelper(List<AdjustmentVoucher> voucherList, List<ItemAdjVoucher> itemVoucherList,
+            List<Item> itemList, List<Employee> employeeList)
+        {
+            List<AdjustmentVoucherVM> vouchersVMList = new List<AdjustmentVoucherVM>();
+            AdjustmentVoucherVM voucherVM = null;
+
+            Employee employee = null;
+
+            List<int> voucherIdsForManager = (from voucherDetail in itemVoucherList
+                                              join items in itemList
+                                              on voucherDetail.ItemId equals items.Id
+                                              select voucherDetail.VoucherId).Distinct().ToList();
+
+            foreach (int id in voucherIdsForManager)
+            {
+                voucherList.RemoveAll(x => x.Id == id);
+            }
+
+            voucherList = voucherList.GroupBy(x => x.Id)
+              .Select(y => y.First())
+              .ToList();
+
+            foreach (AdjustmentVoucher voucher in voucherList)
+            {
+                employee = employeeList.Find(x => x.Id == voucher.EmployeeId);
+
+                voucherVM = new AdjustmentVoucherVM()
+                {
+                    Name = employee.Name,
+                    Id = voucher.Id,
+                    Date = voucher.Date,
+                    Status = voucher.Status,
+                    TotalQuantity = itemVoucherList.Where(x => x.VoucherId == voucher.Id)
+                                    .Sum(y => y.Quantity)
+                };
+
+                vouchersVMList.Add(voucherVM);
+            }
+
+            return vouchersVMList;
+        }
+
+        public List<AdjustmentVoucherVM> GetByStatusForManager(string status)
+        {
+            List<AdjustmentVoucher> voucherList = new List<AdjustmentVoucher>();
+            AdjustmentVoucher av = null;
+
+            List<ItemAdjVoucher> itemVoucherList = new List<ItemAdjVoucher>();
+            ItemAdjVoucher iav = null;
+
+            List<Item> itemList = new List<Item>();
+            Item i = null;
+
+            List<Employee> employeeList = new List<Employee>();
+            Employee employee = null;
+
+            string sql = "SELECT av.*, iav.ID AS [ItemAdjID], iav.ItemID, iav.VoucherID AS [ItemAdjVoucherID], iav.Qty, iav.Reason, i.*, e.ID AS [EmpId], e.Name AS [EmpName] " +
+                          "FROM AdjustmentVoucher av, Item i, ItemAdjVoucher iav, Employee e " +
+                          "WHERE av.VoucherID = iav.VoucherID " +
+                          "AND av.Status=@value " +
+                          "AND e.ID = av.EmployeeID " +
+                          "AND iav.ItemID = i.ID";
+
+            SqlCommand cmd = new SqlCommand(sql, connection);
+
+            cmd.Parameters.Add("@value", SqlDbType.VarChar);
+            cmd.Parameters["@value"].Value = status;
+
+            connection.Open();
+
+            SqlDataReader data = cmd.ExecuteReader();
+
+            while (data.Read())
+            {
+                av = new AdjustmentVoucher()
+                {
+                    Id = Convert.ToInt32(data["VoucherID"]),
+                    EmployeeId = Convert.ToInt32(data["EmployeeID"]),
+                    Date = Convert.ToDateTime(data["Date"]),
+                    Status = data["Status"].ToString()
+                };
+
+                iav = new ItemAdjVoucher()
+                {
+                    Id = Convert.ToInt32(data["ItemAdjVoucherID"]),
+                    ItemId = data["ItemID"].ToString(),
+                    VoucherId = Convert.ToInt32(data["ItemAdjVoucherID"]),
+                    Quantity = Convert.ToInt32(data["Qty"]),
+                    Reason = data["Reason"].ToString()
+                };
+
+                i = new Item()
+                {
+                    Id = data["ID"].ToString(),
+                    CategoryId = Convert.ToInt32(data["CategoryID"]),
+                    Description = data["Description"].ToString(),
+                    ThresholdValue = Convert.ToInt32(data["ThresholdValue"]),
+                    ReorderQty = Convert.ToInt32(data["ReorderQty"]),
+                    Uom = data["UOM"].ToString(),
+                    BinNo = data["BinNo"].ToString(),
+                    FirstSupplier = data["FirstSupplier"].ToString(),
+                    FirstPrice = Convert.ToDouble(data["FirstPrice"]),
+                    SecondSupplier = data["SecondSupplier"].ToString(),
+                    SecondPrice = Convert.ToDouble(data["SecondPrice"]),
+                    ThirdSupplier = data["ThirdSupplier"].ToString(),
+                    ThirdPrice = Convert.ToDouble(data["ThirdPrice"]),
+                };
+
+                employee = new Employee()
+                {
+                    Id = Convert.ToInt32(data["EmpId"]),
+                    Name = data["EmpName"].ToString()
+                };
+
+                voucherList.Add(av);
+                itemVoucherList.Add(iav);
+                itemList.Add(i);
+                employeeList.Add(employee);
+            }
+
+            data.Close();
+            connection.Close();
+
+            voucherList = (from vouchers in voucherList
+                          join voucherDetails in itemVoucherList
+                          on vouchers.Id equals voucherDetails.VoucherId
+                          join item in itemList
+                          on voucherDetails.ItemId equals item.Id
+                          where (item.FirstPrice + item.SecondPrice + item.ThirdPrice) / 3 > 250
+                          select vouchers).ToList();
 
             voucherList = voucherList.GroupBy(x => x.Id)
                           .Select(y => y.First())
@@ -137,47 +299,44 @@ namespace StationeryStore_ADTeam11.DAOs
             }
 
             return vouchersVMList;
-        }        
 
-        public List<AdjustmentVoucherVM> GetByStatusForManager(string status)
-        {
-            List<AdjustmentVoucherVM> voucherList = new List<AdjustmentVoucherVM>();
-            AdjustmentVoucherVM voucher = null;
+            //List<AdjustmentVoucherVM> voucherList = new List<AdjustmentVoucherVM>();
+            //AdjustmentVoucherVM voucher = null;
 
-            string sql = "SELECT e.Name, av.VoucherID, av.Date, av.Status, SUM(iav.Qty) AS TotalQuantity " +
-                        "FROM Item i, AdjustmentVoucher av, ItemAdjVoucher iav, Employee e " +
-                        "WHERE i.ID = iav.ItemID " +
-                        "AND iav.VoucherID = av.VoucherID " +
-                        "AND e.ID = av.EmployeeID " +
-                        "AND av.Status = @value " +
-                        "AND(i.FirstPrice + i.SecondPrice + i.ThirdPrice) / 3 >= 250 " +
-                        " GROUP BY e.Name, av.VoucherID, av.Date, av.Status ";
+            //string sql = "SELECT e.Name, av.VoucherID, av.Date, av.Status, SUM(iav.Qty) AS TotalQuantity " +
+            //            "FROM Item i, AdjustmentVoucher av, ItemAdjVoucher iav, Employee e " +
+            //            "WHERE i.ID = iav.ItemID " +
+            //            "AND iav.VoucherID = av.VoucherID " +
+            //            "AND e.ID = av.EmployeeID " +
+            //            "AND av.Status = @value " +
+            //            "AND(i.FirstPrice + i.SecondPrice + i.ThirdPrice) / 3 >= 250 " +
+            //            " GROUP BY e.Name, av.VoucherID, av.Date, av.Status ";
 
-            SqlCommand cmd = new SqlCommand(sql, connection);
+            //SqlCommand cmd = new SqlCommand(sql, connection);
 
-            connection.Open();
-            cmd.Parameters.Add("@value", SqlDbType.VarChar);
-            cmd.Parameters["@value"].Value = status;
+            //connection.Open();
+            //cmd.Parameters.Add("@value", SqlDbType.VarChar);
+            //cmd.Parameters["@value"].Value = status;
 
-            SqlDataReader data = cmd.ExecuteReader();
+            //SqlDataReader data = cmd.ExecuteReader();
 
-            while (data.Read())
-            {
-                voucher = new AdjustmentVoucherVM()
-                {
-                    Name = data["Name"].ToString(),
-                    Id = Convert.ToInt32(data["VoucherID"]),
-                    Date = Convert.ToDateTime(data["Date"]),
-                    Status = data["Status"].ToString(),
-                    TotalQuantity = Convert.ToInt32(data["TotalQuantity"])
-                };
+            //while (data.Read())
+            //{
+            //    voucher = new AdjustmentVoucherVM()
+            //    {
+            //        Name = data["Name"].ToString(),
+            //        Id = Convert.ToInt32(data["VoucherID"]),
+            //        Date = Convert.ToDateTime(data["Date"]),
+            //        Status = data["Status"].ToString(),
+            //        TotalQuantity = Convert.ToInt32(data["TotalQuantity"])
+            //    };
 
-                voucherList.Add(voucher);
-            }
-            data.Close();
-            connection.Close();
+            //    voucherList.Add(voucher);
+            //}
+            //data.Close();
+            //connection.Close();
 
-            return voucherList;
+            //return voucherList;
         }
 
         public int Add(int employeeId)
